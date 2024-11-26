b1b1e0325f5c0e2d1ca76c0a1f8ffa8346c852c4
Q39547190 <112136869+Q39547190@users.noreply.github.com>
2024-11-26 14:29:21 +0800

    更新 tingche.js

--- tingche.js
+++ tingche.js
@@ -1,7 +1,7 @@
 {
   "name": "新会万达停车🅿️",
-  "version": "1.0.2",
+  "version": "1.0.3",
   "updata": "https://ghp.ci/https://raw.githubusercontent.com/Q39547190/QXG/refs/heads/main/TCdata.js",
-  "upexplain": "新品区的App纳入经典区\n采用服务器➕本地双向解密🎉\n全面提升加载速度☑️\n------------\n•作者:中车大神🔥",
+  "upexplain": "漏洞修复了全面失效😀",
   "Bulletin": "•重构代码\n•修复各种问题\n•全面提升加载速度☑️\n•不定时更新\n🫵2024-11-17更新5个商铺\n正式1.0.2版本😀\n-------------------------\n•作者:中车大神🔥\n版本v1.0.2"
 }
